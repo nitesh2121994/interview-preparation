dddefcf49866af6ed496f50971acd580b0e0593a
Nitesh Sharma <nitesh2121994@gmail.com>
2024-09-30 13:50:40 +0530

    Added flatten object with depth(BFD question)

--- javascript/flatten.js
+++ javascript/flatten.js
@@ -1,32 +1,57 @@
 
 function flattenObj(obj) {
-    const finalObj = {};
-    function flat(currentObj, parentKey) {
-        for (let key in currentObj) {
-            const newParent = parentKey + key ;
-            const value = currentObj[key];
-            if (typeof value === 'object') {
-                flat(value, newParent + '.');
-            } else {
-                finalObj[newParent] = value;
-            }
-        }
+  const finalObj = {};
+  function flat(currentObj, parentKey) {
+    for (let key in currentObj) {
+      const newParent = parentKey + key;
+      const value = currentObj[key];
+      if (typeof value === 'object') {
+        flat(value, newParent + '.');
+      } else {
+        finalObj[newParent] = value;
+      }
     }
+  }
 
-    flat(obj, '');
+  flat(obj, '');
 
-    return finalObj;
+  return finalObj;
 }
 
 const obj = {
-    name: 'Test',
-    address: {
-        city: {
-            area: 'Mumbai'
-        },
-        state: 'Maharashtra',
-        rooms:[3000, 5000]
-    }
+  name: 'Test',
+  address: {
+    city: {
+      area: 'Mumbai'
+    },
+    state: 'Maharashtra',
+    rooms: [3000, 5000]
+  }
 }
 
 console.log(flattenObj(obj));
+
+
+function flatArray(arr, depth = 1) {
+  const results = [];
+
+  const flatArr = (arr, currentDepth) => {
+    if (currentDepth > depth && depth !== Infinity) {
+      results.push(arr);
+    } else {
+      for (let item of arr) {
+        if (Array.isArray(item)) {
+          flatArr(item, currentDepth + 1)
+        } else {
+          results.push(item);
+        }
+      }
+    }
+  }
+
+  flatArr(arr, 0)
+  return results;
+}
+
+console.log("flat", flatArray([1, [2], [3, [4]]], 1));
+console.log("flat", flatArray([1, [[[2]]], [3, [[4]]]], Infinity));
